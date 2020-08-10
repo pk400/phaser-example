aa8239a17d03a63699e29b1606ac169d3f90b996
Joel Aro <joelaro.to@gmail.com>
2020-08-09 22:59:04 -0400

    Initial commit.

---
+++ main.js
@@ -0,0 +1,20 @@
+var game =  new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+})
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+
+}
